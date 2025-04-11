35e35d946abd93f9a2024179b3352844584362a5
Angel Martin <AMartin12122019@outlook.com>
2025-04-11 16:07:45 -0700

    ok hld up

---
+++ net.squish.Bebo/StatusEffects/PoisonEffect.cs
@@ -0,0 +1,9 @@
+// using BrutalAPI;
+//
+// namespace net.squish.Bebo.StatusEffects {
+//     public class PoisonEffect : StatusEffect_SO {
+//         public override bool IsPositive => false;
+//         
+//         public override On
+//     }
+// }
---
+++ net.squish.Bebo/StatusEffects/Statuses.cs
@@ -0,0 +1,7 @@
+namespace net.squish.Bebo.StatusEffects {
+    public class Statuses {
+        public static void Add() {
+            
+        }
+    }
+}
